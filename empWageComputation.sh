f63c6e569d5bd14149c4252bce86872984b98c03
amtksms97 <amtksms97@gmail.com>
2020-11-04 10:42:55 +0530

    Added: Employee Partime hours and wage in Employee Wage program

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,18 @@
 echo "Welcome to Employee Wage Computation Program"
 WagePerHour=20
 FullDayHour=8
+PartimeHour=4
 attendance=$((RANDOM%2))
+partime=$((RANDOM%2))
 if(( $attendance == 1 ))
 then
 	echo "Employee is Present"
+	if(( $partime == 1 ))
+	then
+		echo "Employee is working Fulltime"
+	else
+		echo "Employee is working Halftime"
+	fi
 else
 	echo "Employee is Absent"
 fi
