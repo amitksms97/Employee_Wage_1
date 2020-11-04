462c8ff3f97d9a99b36820ee4253324557e9b4bb
amtksms97 <amtksms97@gmail.com>
2020-11-04 11:28:52 +0530

    Added: Chnaged the conditions from if statement to switch statement in Employee wage program

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,15 +4,20 @@ FullDayHour=8
 PartimeHour=4
 attendance=$((RANDOM%2))
 partime=$((RANDOM%2))
-if(( $attendance == 1 ))
-then
-	echo "Employee is Present"
-	if(( $partime == 1 ))
-	then
-		echo "Employee is working Fulltime"
-	else
-		echo "Employee is working Halftime"
-	fi
-else
-	echo "Employee is Absent"
-fi
+case $attendance in
+	1)
+		echo "Employee is Present"
+		if(( $partime == 1 ))
+		then
+			echo "Employee is working Fulltime"
+		else
+			echo "Employee is working Halftime"
+		fi
+		;;
+	0)
+		echo "Employee is Absent"
+		;;
+	*)
+		echo "Invalid Entry"
+		;;
+esac
