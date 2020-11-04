3b08d5c20863edb4e520747593ad63b9432eac31
amtksms97 <amtksms97@gmail.com>
2020-11-04 14:51:01 +0530

    All compuation Added and stored and printed as output

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,48 +4,37 @@ WagePerHour=20
 FullDayHour=8
 PartimeHour=4
 days=0
-while [[ $days -ne 20 && $Totalhours -ne 100 ]]
-do
-attendance=$((RANDOM%2))
-partime=$((RANDOM%2))
-((days++))
+echo -e "Day No. \t DailyWage \t TotalWage"
 while [[ $Days -ne 20 && $TotalHours -ne 100 ]]
 do
 attendance=$((RANDOM%2))
 partime=$((RANDOM%2))
 ((Days++))
-case $attendance in
-	1)
-		echo "Employee is Present"
-		if(( $partime == 1 ))
-		then
-			echo "Employee is working Fulltime"
-
-			Hours=$FullDayHour
-		else
-			echo "Employee is working Halftime"
-			wage=`expr $WagePerHour \* $FullDayHour`
-			Hours=$FullDayHour
-		else
-			echo "Employee is working Halftime"
-			wage=`expr $WagePerHour \* $PartimeHour`
-			Hours=$PartimeHour
-		fi
-		;;
-	0)
-		echo "Employee is Absent"
-		Hours=0
+	case $attendance in
+		1)
+			echo "Employee is Present"
+			if(( $partime == 1 ))
+			then
+				echo "Employee is working Fulltime"
+				wage=`expr $WagePerHour \* $FullDayHour`
+				Hours=$FullDayHour
+			else
+				echo "Employee is working Halftime"
+				wage=`expr $WagePerHour \* $PartimeHour`
+				Hours=$PartimeHour
+			fi
+			;;
+		0)
+			echo "Employee is Absent"
+			Hours=0
 			wage=0
 			Hours=0
-		;;
-	*)
-		echo "Invalid Entry"
-		;;
-esac
-Totalhours=`expr $Totalhours + $Hours`
+			;;
+		*)
+			echo "Invalid Entry"
+			;;
+	esac
 TotalHours=`expr $TotalHours + $Hours`
 TotalWage=`expr $TotalWage + $wage`
-echo $TotalHours
-echo $TotalWage
-echo $Days
+echo -e "$Days \t $wage \t $TotalWage"
 done
