0d2d92487001c3878c3cff25152f997eb07ffd9f
amtksms97 <amtksms97@gmail.com>
2020-11-04 13:18:24 +0530

    Added: Daily Wage and Total Wage Calculation in Employee Wage Program

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,25 +3,37 @@ echo "Welcome to Employee Wage Computation Program"
 WagePerHour=20
 FullDayHour=8
 PartimeHour=4
-for (( i=1; i<=20; i++ ))
+while [[ $Days -ne 20 && $TotalHours -ne 100 ]]
 do
 attendance=$((RANDOM%2))
 partime=$((RANDOM%2))
+((Days++))
 case $attendance in
 	1)
 		echo "Employee is Present"
 		if(( $partime == 1 ))
 		then
 			echo "Employee is working Fulltime"
+			wage=`expr $WagePerHour \* $FullDayHour`
+			Hours=$FullDayHour
 		else
 			echo "Employee is working Halftime"
+			wage=`expr $WagePerHour \* $PartimeHour`
+			Hours=$PartimeHour
 		fi
 		;;
 	0)
 		echo "Employee is Absent"
+			wage=0
+			Hours=0
 		;;
 	*)
 		echo "Invalid Entry"
 		;;
 esac
+TotalHours=`expr $TotalHours + $Hours`
+TotalWage=`expr $TotalWage + $wage`
+echo $TotalHours
+echo $TotalWage
+echo $Days
 done
